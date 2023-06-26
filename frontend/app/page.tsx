f2f572f29024db703bf70b78c0d31590f69589e9
Hugo <swenkkros@gmail.com>
2023-06-26 16:57:24 +0200

    Installed react-hot-toast

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,10 +2,12 @@ import RootLayout from "./layout";
 import MainLayout from "@/components/MainLayout";
 import HeroSection from "@/components/HeroSection";
 import ParticipantsTable from "@/components/ParticipantsTable/ParticipantsTable";
+import { Toaster } from "react-hot-toast";
 
 export default function Home() {
   return (
     <RootLayout>
+      <Toaster />
       <MainLayout>
         <HeroSection />
         <ParticipantsTable />
